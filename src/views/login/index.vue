19ba5f6882b1ba6f3073aa42e46a10c0849216bc
wenjin_deng <wenjin_deng@126.com>
2020-08-25 20:54:34 +0800

    validate

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,14 +6,16 @@
         <van-field v-model="user.mobile" required label="用户名" placeholder="请输入用户名" />
         <van-field v-model="user.code" required label="验证码" placeholder="请输入验证码" type="password" />
       </van-cell-group>
-      <van-button type="info" block @click.prevent="toLogin()">登录</van-button>
+      <van-button
+      :loading="isLogin"
+      type="info" block @click.prevent="toLogin()">登录</van-button>
     </form>
   </div>
 </template>
 
 <script>
 import { login } from '@/api/user.js'
-import { setUser } from '@/utils/auth.js'
+import { mapMutations } from 'vuex'
 
 export default {
   name: 'LoginIndex',
@@ -22,14 +24,18 @@ export default {
       user: {
         mobile: '18801185985',
         code: '246810'
-      }
+      },
+      isLogin: false
     }
   },
   methods: {
+    ...mapMutations(['setUser']),
     async toLogin () {
       try {
+        this.isLogin = true
         const res = await login(this.user)
-        setUser(res)
+        this.setUser(res)
+        this.isLogin = false
       } catch (error) {
         console.log(error)
       }
